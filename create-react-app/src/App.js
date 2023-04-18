84ca996a305ef7ca0f50dfac7eb302985b3c013e
votanak <v0tanak@yandex.ru>
2023-04-19 00:14:03 +0300

    asdf

--- create-react-app/src/App.js
+++ create-react-app/src/App.js
@@ -1,32 +1,16 @@
 import React, { useState } from 'react';
 
-function App() {
-  const [value1, setValue1] = useState(0);
-  const [value2, setValue2] = useState(0);
-  const [result, setResult] = useState(0);
+function App(props) {
+  const [checkboxChecked, setCheckboxChecked] = useState(false);
+  const checkHandle = () => {
+    setCheckboxChecked(!checkboxChecked);
+  };
 
-  function handleChange1(event) {
-    setValue1(event.target.value);
-  }
-
-  function handleChange2(event) {
-    setValue2(event.target.value);
-  }
-
-  function handleClickResult(event) {
-    console.log(event.target.innerText);
-    event.target.innerText === 'summ'
-      ? setResult(Number(value1) + Number(value2))
-      : setResult(Number(value1) * Number(value2));
-  }
   return (
     <div>
-      <input value={value1} onChange={handleChange1} />
-      <input value={value2} onChange={handleChange2} />
-
-      <button onClick={handleClickResult}>summ</button>
-      <button onClick={handleClickResult}>proizv</button>
-      <p>result: {result}</p>
+      <input type="checkbox" onChange={checkHandle} />
+      <label>Вам есть 18 лет?</label>
+      {!checkboxChecked && <p>увы, вам еще нет 18 лет</p>}
     </div>
   );
 }
