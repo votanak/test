32229a279040b6939eb8d696f709e199c7ec6eb7
votanak <v0tanak@yandex.ru>
2023-11-04 23:36:59 +0300

    sdkfs

--- index.js
+++ index.js
@@ -1,18 +1,1 @@
-function sum(a, b, c) {
-  return a + b + c;
-}
-
-function multi(a, b, c) {
-  return a * b * c;
-}
-console.log(sum.length);
-
-function curry(fn) {
-  return function (...args) {};
-}
-
-const curriedSum = curry(sum);
-const currieMulti = curry(multi);
-curriedSum(2, 3, 4);
-curriedSum(2)(3)(4);
-curriedMulti(2, 3)(4);
+const request = new XMLHttpRequest();
