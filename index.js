dc0eb63f50990a3fb4236dbe39256a605b19102e
votanak <v0tanak@yandex.ru>
2023-11-06 00:41:19 +0300

    sdfsf

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-fetch("users/anton.json")
-  .then((response) => {
-    // console.log("Успешно:", response);
-    return response.json();
-  })
+const fetchUsers = async (url) => {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+};
+
+fetchUsers("users/kirill.json")
   .then((data) => {
-    console.log(data);
+    console.log("Успешно", data);
   })
   .catch((error) => {
-    console.log("Провал:", error);
+    console.log("Ошибочка:", error);
   });
