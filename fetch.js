35f44f982edbad40e3b218b3672c692a5336b9d0
votanak <v0tanak@yandex.ru>
2023-11-14 21:02:17 +0300

    laskdj

--- fetch.js
+++ fetch.js
@@ -1,6 +1,12 @@
-const promise = new Promise((resolve, reject) => {
-  resolve("success");
-});
-promise.then((res) => {
-  console.log(res);
-});
+const myRequest = new Request("del1.png");
+
+fetch(myRequest)
+  .then((response) => {
+    console.log(response); // response.status = 200
+    return response.blob();
+  })
+  .then((myBlob) => {
+    const objectURL = URL.createObjectURL(myBlob);
+    myImage.src = objectURL;
+  })
+  .catch((e) => console.log(e));
