0f2d59c969dbb9934ec620b35450a2ca350faa1b
votanak <v0tanak@yandex.ru>
2023-11-16 22:50:12 +0300

    sadfas

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-const fetchUsers = async (url) => {
-  const response = await fetch(url);
-  const data = await response.json();
-  return data;
-};
+function memoize(fn) {
+  let memRes = {};
+  return function (...args) {
+    if (typeof memRes[args.toString()] === "undefined") {
+      newVal = fn(...args);
+      memRes = { ...memRes, [args.toString()]: newVal };
+      return newVal;
+    } else {
+      console.log(memRes);
+      return memRes[args.toString()];
+    }
+  };
+}
 
-fetchUsers("users/kirill.json")
-  .then((data) => {
-    console.log("Успешно", data);
-  })
-  .catch((error) => {
-    console.log("Ошибочка:", error);
-  });
+let callCount = 0;
+const memoizedFn = memoize(function (a, b) {
+  callCount += 1;
+  return a + b;
+});
+console.log(memoizedFn(0, 0));
+console.log(memoizedFn(0, 0));
+console.log(callCount);
