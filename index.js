533a91112da748af9ab9dfe4903acabf06baa147
votanak <v0tanak@yandex.ru>
2023-11-26 00:29:27 +0300

    asdfas

--- index.js
+++ index.js
@@ -1,41 +1,58 @@
-var values = [];
 var TimeLimitedCache = function () {
-  setInterval(() => {
-    let newValues = [];
-    values.forEach((el, ind) => {
-      el[2] -= 1;
-      if (el[2] <= 0) newValues.push(el[0]);
-    }, 1);
-    values = values.filter((el) => newValues.includes(el[0]));
-  });
+  this.values = [];
+  let inter = setInterval(() => {
+    console.log("interval", this.values);
+  }, 1);
 };
 
 TimeLimitedCache.prototype.set = function (key, value, duration) {
-  values.forEach((el, ind) => {
-    if (el[0] === key[0]) {
-      el[0] = [key, value, duration];
-      return true;
+  let res;
+  this.values.forEach((el) => {
+    if (el[0] === key) {
+      el = [key, value, duration];
+      res = true;
     }
   });
-  values.push([key, value, duration]);
+  if (res) {
+    return true;
+  }
+  this.values.push([key, value, duration]);
+  setTimeout(() => {
+    this.values = [...this.values.filter((el) => el[0] !== key)];
+  }, duration);
   return false;
 };
 
 TimeLimitedCache.prototype.get = function (key) {
-  values.forEach((el, ind) => {
-    if (el[0] === key[0]) return el[1];
+  let res;
+  this.values.forEach((el) => {
+    if (el[0] == key) {
+      res = el[1];
+      return;
+    }
   });
+  if (res) return res;
   return -1;
 };
 
-/**
- * @return {number} count of non-expired keys
- */
 TimeLimitedCache.prototype.count = function () {
-  return values.length;
+  return this.values.length;
 };
 
 const timeLimitedCache = new TimeLimitedCache();
-timeLimitedCache.set(1, 42, 1000); // false
-timeLimitedCache.get(1); // 42
-timeLimitedCache.count(); // 1
+console.log(timeLimitedCache.set(1, 42, 50)); // false
+setTimeout(() => {
+  console.log("Запрос 1", timeLimitedCache.set(1, 42, 100)); // false
+}, 40);
+setTimeout(() => {
+  console.log("Запрос 2", timeLimitedCache.get(1)); // 42
+}, 50);
+setTimeout(() => {
+  console.log("Запрос 3", timeLimitedCache.get(1)); // 42
+}, 120);
+setTimeout(() => {
+  console.log("Запрос 4", timeLimitedCache.get(1)); // 42
+}, 200);
+setTimeout(() => {
+  console.log("Запрос 5", timeLimitedCache.count()); // 1
+}, 250);
