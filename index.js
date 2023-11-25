2b34ed69f9d97fc875ec85cab998b3533e062e60
votanak <v0tanak@yandex.ru>
2023-11-25 20:33:59 +0300

    alskd

--- index.js
+++ index.js
@@ -1,22 +1,41 @@
-function memoize(fn) {
-  let memRes = {};
-  return function (...args) {
-    if (typeof memRes[args.toString()] === "undefined") {
-      newVal = fn(...args);
-      memRes = { ...memRes, [args.toString()]: newVal };
-      return newVal;
-    } else {
-      console.log(memRes);
-      return memRes[args.toString()];
-    }
-  };
-}
+var values = [];
+var TimeLimitedCache = function () {
+  setInterval(() => {
+    let newValues = [];
+    values.forEach((el, ind) => {
+      el[2] -= 1;
+      if (el[2] <= 0) newValues.push(el[0]);
+    }, 1);
+    values = values.filter((el) => newValues.includes(el[0]));
+  });
+};
 
-let callCount = 0;
-const memoizedFn = memoize(function (a, b) {
-  callCount += 1;
-  return a + b;
-});
-console.log(memoizedFn(0, 0));
-console.log(memoizedFn(0, 0));
-console.log(callCount);
+TimeLimitedCache.prototype.set = function (key, value, duration) {
+  values.forEach((el, ind) => {
+    if (el[0] === key[0]) {
+      el[0] = [key, value, duration];
+      return true;
+    }
+  });
+  values.push([key, value, duration]);
+  return false;
+};
+
+TimeLimitedCache.prototype.get = function (key) {
+  values.forEach((el, ind) => {
+    if (el[0] === key[0]) return el[1];
+  });
+  return -1;
+};
+
+/**
+ * @return {number} count of non-expired keys
+ */
+TimeLimitedCache.prototype.count = function () {
+  return values.length;
+};
+
+const timeLimitedCache = new TimeLimitedCache();
+timeLimitedCache.set(1, 42, 1000); // false
+timeLimitedCache.get(1); // 42
+timeLimitedCache.count(); // 1
