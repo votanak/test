e84a3eabe54b48b74e6765ce69589e1b8ad7fff7
votanak <v0tanak@yandex.ru>
2023-11-27 02:18:47 +0300

    sdfs

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-var debounce = function (fn, t) {
-  let called = false;
-  return function (...args) {
-    called = true;
-    return setTimeout(() => {
-      console.log(called);
-      if (called) {
-        fn(...args);
-        called = false;
-      }
-    }, t);
-  };
-};
+var functions = [
+  () => new Promise((resolve) => setTimeout(() => resolve(4), 50)),
+  () => new Promise((resolve) => setTimeout(() => resolve(10), 1500)),
+  () => new Promise((resolve) => setTimeout(() => resolve(16), 100)),
+];
 
-const log = debounce(console.log, 500);
-log("Hello 0");
-setTimeout(() => log("Hello 2000"), 2000);
-setTimeout(() => log("Hello 1000"), 1000);
+var promiseAll = function (functions) {
+  const start = new Data();
+  let inter = setInterval(() => {}, 1);
+  // return  Promise((res) => {
+  let as = async () => {
+    let s0 = await functions[0]();
+    let s1 = await functions[1]();
+    let s2 = await functions[2]();
+    return [s0, s1, s2];
+  };
+  // });
+};
