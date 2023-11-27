8284dad4c405d678d708fae3c82f84b9e8be9493
votanak <v0tanak@yandex.ru>
2023-11-27 22:43:25 +0300

    sdfs

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-var functions = [
+var funcs = [
   () => new Promise((resolve) => setTimeout(() => resolve(4), 50)),
   () => new Promise((resolve) => setTimeout(() => resolve(10), 1500)),
   () => new Promise((resolve) => setTimeout(() => resolve(16), 100)),
 ];
 
-var promiseAll = function (functions) {
-  const start = new Data();
-  let inter = setInterval(() => {}, 1);
-  // return  Promise((res) => {
-  let as = async () => {
-    let s0 = await functions[0]();
-    let s1 = await functions[1]();
-    let s2 = await functions[2]();
-    return [s0, s1, s2];
-  };
-  // });
+var promiseAll = async function (functions) {
+  let mass = [];
+  for (let i = 0; i < functions.length; i += 1) {
+    async () => {
+      const el = await functions[i]();
+      mass.push(el);
+    };
+  }
+  console.log(mass);
+  return;
 };
+
+promiseAll(funcs);
