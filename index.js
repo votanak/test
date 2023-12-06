0163b05f2400e56c6395a308d12ef12b93bcc106
votanak <v0tanak@yandex.ru>
2023-12-07 02:43:11 +0300

    sdfs

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
-var funcs = [
-  () => new Promise((resolve) => setTimeout(() => resolve(4), 50)),
-  () => new Promise((resolve) => setTimeout(() => resolve(10), 1500)),
-  () => new Promise((resolve) => setTimeout(() => resolve(16), 100)),
-];
-
-var promiseAll = async function (functions) {
-  let mass = [];
-  for (let i = 0; i < functions.length; i += 1) {
-    try {
-      functions[i]();
-    } catch {
-      return;
-    }
-    mass.push(el);
-  }
-  console.log(mass);
+var join = function (arr1, arr2) {
+  arr2.forEach((el2, ind2) => {
+    let found = false;
+    arr1.forEach((el1, ind1) => {
+      if (el1.id === el2.id) {
+        arr1[ind1][key] = arr2[ind2][key];
+        found = true;
+      }
+    });
+    if (!found) arr1.push(el2);
+  });
+  return arr1.sort((a, b) => a.id - b.id);
 };
-
-promiseAll(funcs);
+arr1 = [{ id: 1, b: { b: 94 }, v: [4, 3], y: 48 }];
+arr2 = [{ id: 1, b: { c: 84 }, v: [1, 3] }];
+console.log(join(arr1, arr2));
