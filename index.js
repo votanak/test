ceb1d8d300251b0c7d2c101b21c99a764c1de98b
votanak <v0tanak@yandex.ru>
2023-12-08 00:11:02 +0300

    sdfas

--- index.js
+++ index.js
@@ -1,16 +1,52 @@
+// var join = function (arr1, arr2) {
+//   arr2.forEach((obj2, ind2) => {
+//     // цикл по второму массиву obj2 - объект из массива arr2
+//     let found = false;
+//     arr1.forEach((obj1, ind1) => {
+//       // проходимся по объектам первого массива obj1 - объект из массива arr1
+//       if (obj1.id === obj2.id) {
+//         // если id совпадает, то ...
+//         for (el in obj2) {
+//           // ... записываем все пары объекта из arr2 в объект из arr1
+//           obj1[el] = obj2[el];
+//         }
+//         found = true; //флаг нахождения obj2 в arr1
+//       }
+//     });
+//     if (!found) arr1.push(obj2);
+//   });
+//   return arr1.sort((a, b) => a.id - b.id);
+// };
+
 var join = function (arr1, arr2) {
-  arr2.forEach((el2, ind2) => {
+  let arr1Mod = arr1.map((el) => {
+    const { id, ...rest } = el;
+    return { [id]: rest };
+  });
+  let arrId1;
+  let arrId2 = arr2.map((el) => el.id);
+  // let notEq = arrId1.filter((el) => !arrId2.includes(el));
+  console.log(arr1Mod);
+  // console.log('notEq', notEq);
+  arr2.forEach((obj2, ind2) => {
+    // цикл по второму массиву obj2 - объект из массива arr2
     let found = false;
-    arr1.forEach((el1, ind1) => {
-      if (el1.id === el2.id) {
-        arr1[ind1][key] = arr2[ind2][key];
-        found = true;
+    arr1.forEach((obj1, ind1) => {
+      // проходимся по объектам первого массива obj1 - объект из массива arr1
+      if (obj1.id === obj2.id) {
+        // если id совпадает, то ...
+        for (el in obj2) {
+          // ... записываем все пары объекта из arr2 в объект из arr1
+          obj1[el] = obj2[el];
+        }
+        found = true; //флаг нахождения obj2 в arr1
       }
     });
-    if (!found) arr1.push(el2);
+    if (!found) arr1.push(obj2);
   });
   return arr1.sort((a, b) => a.id - b.id);
 };
+
 arr1 = [{ id: 1, b: { b: 94 }, v: [4, 3], y: 48 }];
 arr2 = [{ id: 1, b: { c: 84 }, v: [1, 3] }];
 console.log(join(arr1, arr2));
