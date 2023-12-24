b2afc4c5cae15b6f0cb57ddcee35856984b41a1d
votanak <v0tanak@yandex.ru>
2023-12-25 02:10:53 +0300

    sdf

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 arr = [1, 2, 3, [4, 5, 6], [7, 8, [9, 10, 11], 12], [13, 14, 15]];
 var flat = function (arr, n) {
-  for (i = 1; i<=n; i++) {
-      for (j = 1; j<arr.length; j++){
-        arr[j] = ...arr[j]
-      }
-  }
+  for (i = 1; i <= n; i++)
+    if (arr.isArray()) {
+      newArr.push([...arr.Slice(1, j)]);
+      newArr.push(...arr[j]);
+      newArr.push([...arr.Slice(j + 1, arr.length)]);
+      i -= 1;
+    } else {
+    }
 };
 
 console.log(flat(arr));
