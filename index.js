63777e8b4a66fbabbec900c1593a2a8fd1242cbc
votanak <v0tanak@yandex.ru>
2023-12-26 23:49:47 +0300

    asdfasd

--- index.js
+++ index.js
@@ -1,53 +1,6 @@
-var join = function (arr1, arr2) {
-  arr1.forEach(JSON.stringify(arr1));
-
-  arr1.forEach((obj1) => {
-    if (arr2[0].includes(obj1))
-      if (obj1.id === obj2.id) {
-        for (el in obj2) {
-          obj1[el] = obj2[el];
-        }
-      }
-  });
-  //   if (!found) arr1.push(obj2){
-
-  // };
-  return arr1.sort((a, b) => a.id - b.id);
-};
-
-// var join = function (arr1, arr2) {
-//   let obj1 = arr1.reduce((acc, el) => {
-//     const { id, ...rest } = el;
-//     acc[id] = rest;
-//     return acc;
-//   }, {});
-//   let obj2 = arr2.reduce((acc, el) => {
-//     const { id, ...rest } = el;
-//     acc[id] = rest;
-//     return acc;
-//   }, {});
-
-//   console.log(obj1);
-
-//   for (key of Object.keys(obj1)) {
-//     if (Object.keys(obj2).includes(key)) {
-//       for (keyIn of Object.keys(obj1[key])) {
-//         if (!Object.keys(obj2[key]).includes(keyIn)) {
-//           obj2[key][keyIn] = obj1[key][keyIn];
-//         }
-//       }
-//     } else {
-//       obj2[key] = obj1[key];
-//     }
-//   }
-//   let newArr = Object.keys(obj2).reduce((acc, el, ind) => {
-//     acc.push({ id: +el, ...obj2[el] });
-//     return acc;
-//   }, []);
-//   return newArr.sort((a, b) => a.id - b.id);
-// };
-
-arr1 = [{ id: 1, b: { b: 94 }, v: [4, 3], y: 48 }];
-arr2 = [{ id: 1, b: { c: 84 }, v: [1, 3] }];
-console.log(join(arr1, arr2));
-// console.log(Object.values(arr1[0]));
+fetch(
+  'http://192.168.1.65:8000/event_info?event_id=676959&user_token=1702901399752-134904',
+  {
+    method: 'GET',
+  },
+);
