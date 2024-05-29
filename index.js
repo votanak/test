c71585fe24c04baf44a0e541398d765928a386ad
votanak <v0tanak@yandex.ru>
2024-05-29 04:14:13 +0300

    ыва

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const img = new Image();
-let div = document.createElement('div');
-div.className = 'alert';
-img.src =
-  'https://sun1-57.userapi.com/s/v1/if1/_Y_4QMdFCSQLyVdHkEmHRCd93FIxLhjo_50HHAKmnMi72R9sdOPKIWTlMM09gPku55U0M12O.jpg?size=960x1476&quality=96&crop=0,0,960,1476&ava=1';
+const divOpen = document.getElementById('divopen');
+var select = document.getElementById('select');
 
-img.onload = function () {
-  div.innerHTML = `ширина: ${this.width} + 'x' + ${this.height}`;
-  document.body.append(div);
-  document.body.append(img);
-};
+const evt = new FocusEvent('focus', {
+  bubbles: true,
+  cancelable: true,
+  view: window,
+});
+
+divOpen.addEventListener('click', function () {
+  console.log('pressed');
+  select.focus();
+  select.click();
+  select.dispatchEvent(evt);
+});
