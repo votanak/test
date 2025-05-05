bea02fc6855e384d1bfaf975c95376f8a4e94dcb
votanak <v0tanak@yandex.ru>
2025-05-05 23:35:07 +0300

    zdsad

--- temp.js
+++ temp.js
@@ -1,7 +1,21 @@
-const timeout = (time) => {
-  setTimeout(() => {
-    console.log('время пошло...');
-  }, time);
-  return 'Hello';
-};
-timeout(2000);
+function getCompressedString(text) {
+  let text1;
+  text1 = text.split(' ').join('_ _');
+  text1 = text1.split(',').join('_,_');
+  text1 = text1.split('.').join('_._');
+  text1 = text1.split('?').join('_?_');
+  text1 = text1.split('!').join('_!_');
+  fullArr = text1.split('_').filter((el) => el !== '');
+  wordArr = fullArr.filter(
+    (el) => el !== ' ' && el !== '.' && el !== ',' && el != '?' && el !== '!',
+  );
+  indexArr = wordArr.map((el, i, arr) =>
+    arr.reduce((acc, word) => (word === el ? acc + 1 : acc), 0),
+  );
+  return indexArr;
+}
+
+console.log(
+  getCompressedString(`Привет, как у тебя дела?
+Да, вроде, хорошо, а у тебя?`),
+);
