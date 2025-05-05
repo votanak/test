59b10eb0d13b3b706f0905b6b66afec3acdbebba
votanak <v0tanak@yandex.ru>
2025-05-06 01:16:08 +0300

    adsfasd

--- temp.js
+++ temp.js
@@ -1,21 +1,35 @@
-function getCompressedString(text) {
-  let text1;
-  text1 = text.split(' ').join('_ _');
-  text1 = text1.split(',').join('_,_');
-  text1 = text1.split('.').join('_._');
-  text1 = text1.split('?').join('_?_');
-  text1 = text1.split('!').join('_!_');
-  fullArr = text1.split('_').filter((el) => el !== '');
-  wordArr = fullArr.filter(
-    (el) => el !== ' ' && el !== '.' && el !== ',' && el != '?' && el !== '!',
-  );
-  indexArr = wordArr.map((el, i, arr) =>
-    arr.reduce((acc, word) => (word === el ? acc + 1 : acc), 0),
-  );
-  return indexArr;
-}
+  function getCompressedString(text) {
+    let text1;
+    let wordObj = {};
+    text1 = text.split(' ').join('_ _');
+    text1 = text1.split(',').join('_,_');
+    text1 = text1.split('.').join('_._');
+    text1 = text1.split('?').join('_?_');
+    text1 = text1.split('!').join('_!_');
+    fullArr = text1.split('_').filter((el) => el !== '');
+    wordArr = fullArr.filter(
+      (el) => el !== ' ' && el !== '.' && el !== ',' && el != '?' && el !== '!',
+    );
+    indexArr = wordArr
+      .map((el, i, arr) => [
+        el,
+        arr.reduce((acc, word) => (word === el ? acc + 1 : acc), 0),
+      ])
+      .sort((a, b) => b[1] - a[1]);
+    indexArrUnique = [...new Set(indexArr.map(JSON.stringify))].map(JSON.parse);
+    console.log('ful: ', fullArr);
 
-console.log(
-  getCompressedString(`Привет, как у тебя дела?
-Да, вроде, хорошо, а у тебя?`),
-);
+    res = fullArr
+      .map((el) =>
+        indexArrUnique.findIndex((el1) => el === el1[0]) >= 0
+          ? indexArrUnique.findIndex((el1) => el === el1[0])
+          : el,
+      )
+      .join('');
+    return res;
+  }
+
+  console.log(
+    getCompressedString(`Привет, как у тебя дела?
+  Да, вроде, хорошо, а у тебя?`),
+  );
