cab3602144c4ac52f1d78ae7650c0e01ad3beac0
votanak <v0tanak@yandex.ru>
2025-05-11 00:46:17 +0300

    asdad

--- temp.js
+++ temp.js
@@ -1,21 +1,3 @@
-function getCompressedString(text) {
-  let text1;
-  text1 = text.split(' ').join('_ _');
-  text1 = text1.split(',').join('_,_');
-  text1 = text1.split('.').join('_._');
-  text1 = text1.split('?').join('_?_');
-  text1 = text1.split('!').join('_!_');
-  fullArr = text1.split('_').filter((el) => el !== '');
-  wordArr = fullArr.filter(
-    (el) => el !== ' ' && el !== '.' && el !== ',' && el != '?' && el !== '!',
-  );
-  indexArr = wordArr.map((el, i, arr) =>
-    arr.reduce((acc, word) => (word === el ? acc + 1 : acc), 0),
-  );
-  return indexArr;
-}
-
-console.log(
-  getCompressedString(`Привет, как у тебя дела?
-Да, вроде, хорошо, а у тебя?`),
-);
+const lastSeen = new Map();
+lastSeen.set('a', 10);
+console.log(lastSeen.get('b'));
