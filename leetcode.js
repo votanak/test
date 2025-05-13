bd2f8ebf9740d1683574f410ab3a86fa620b4c06
votanak <v0tanak@yandex.ru>
2025-05-13 19:59:32 +0300

    9283983

--- leetcode.js
+++ leetcode.js
@@ -1,18 +1,18 @@
 var convert = function (s, numRows) {
-  const strings = [];
-  for (j = 0; j < numRows; j++) {
-    strings.push([]);
+  if (numRows === 1) {
+    return s;
   }
+  const strings = new Array(numRows);
+  strings.fill('');
   let sNumber = 0;
   let sStep = 1;
   for (i = 0; i < s.length; i++) {
-    strings[sNumber].push(s[i]);
+    strings[sNumber] += s[i];
     sNumber = sNumber + sStep;
     if (sNumber === numRows - 1) sStep = -1;
     if (sNumber === 0) sStep = 1;
-    console.log(strings);
   }
-  return strings.toString().replaceAll(',', '');
+  return strings.join('');
 };
 
-console.log(convert('ab', 3));
+console.log(convert('ablkaj,alsdkjfskf,sdfsafdjlsa', 5));
