0be855f55b1a485bddf4b98aacb449bec3ce8820
votanak <v0tanak@yandex.ru>
2025-05-22 00:48:03 +0300

    rttytd

--- leetcode.js
+++ leetcode.js
@@ -1,5 +1,12 @@
-var func = function (s, p) {
-  if (!(p.includes('.') || p.includes('*'))) return s === p;
-  return new RegExp(`^${p}$`, 'g').test(s);
+var func = function (strs) {
+  res = '';
+  len = Math.min(...strs.map((s) => s.length));
+  for (i = 0; i < len; i++) {
+    for (j = 0; j < strs.length; j++) {
+      if (strs[j][i] !== strs[0][i]) return res;
+    }
+    res = res + strs[0][i];
+  }
+  return res;
 };
-console.log(func(3749)); //MMMDCCXLIX
+console.log(func(['flower', 'flow', 'flight'])); //
